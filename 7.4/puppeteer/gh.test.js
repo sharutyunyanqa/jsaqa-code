762562987e0ad6e22ce2ccc7e49bfc25d5361548
Sona <s.harutyunyan1992@gmail.com>
2024-02-16 19:41:23 +0400

    Puppeteer homework

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -11,24 +11,44 @@ afterEach(() => {
 
 describe("Github page tests", () => {
   test("The h1 header content'", async () => {
-    const firstLink = await page.$("header div div a");
-    await firstLink.click();
-    await page.waitForSelector('h1');
-    const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+      const firstLink = await page.$("header div div a");
+      await firstLink.click();
+      await page.waitForSelector('h1');
+      const title2 = await page.title();
+      expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+  }, 60000); 
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
-    expect(actual).toEqual("#start-of-content");
+      const actual = await page.$eval("a", link => link.getAttribute('href') );
+      expect(actual).toEqual("#start-of-content");
+  }, 30000); 
+  test("The page contains Sign in button", async () => {
+      const btnSelector = ".btn-large-mktg.btn-mktg";
+      await page.waitForSelector(btnSelector, {
+          visible: true,
+      });
+      const actual = await page.$eval(btnSelector, link => link.textContent);
+      expect(actual).toContain("Sign up for free")
+  }, 45000); 
+});
+describe("Should test other menu sections", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
   });
 
-  test("The page contains Sign in button", async () => {
-    const btnSelector = ".btn-large-mktg.btn-mktg";
-    await page.waitForSelector(btnSelector, {
-      visible: true,
-    });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
-  });
+  test("Go to Pricing", async () => {
+    await page.goto("https://github.com/pricing");
+    const title = await page.title();
+    expect(title).toContain("Pricing · Plans for every developer · GitHub");
+  }, 6000);
+  test("Go to Enterprise", async () => {
+    await page.goto("https://github.com/enterprise");
+    const title = await page.title();
+    expect(title).toContain("The AI Powered Developer Platform. · GitHub");
+  },6000);
+  test("Go to Sponsors",async () => {
+    await page.goto("https://github.com/sponsors");
+    const title = await page.title();
+    expect(title).toContain("GitHub Sponsors · GitHub");
+  },6000);
 });
